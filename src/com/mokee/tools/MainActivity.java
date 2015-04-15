47b3ad6f4ce336dc76bb32f8a87d80bc12451c43
zhangxiangqian <821487678@qq.com>
2015-04-15 10:37:25 +0800

    程序的创建，Phone的布局

---
+++ src/com/mokee/tools/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mokee.tools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
